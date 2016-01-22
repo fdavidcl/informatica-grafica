793406846d74bd614e00ae607be919edae3b7585
David Charte <fdavidcl@outlook.com>
2016-01-22 01:20:20 +0100

    Fuentes de luz

---
+++ FuenteLuz.cpp
@@ -0,0 +1,67 @@
+#include "FuenteLuz.hpp"
+
+void ColeccionFL::activar() {
+  glEnable(GL_LIGHTING);
+  glEnable(GL_NORMALIZE);
+  glDisable(GL_COLOR_MATERIAL);
+  glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+  glLightModeli(GL_LIGHT_MODEL_COLOR_CONTROL, GL_SEPARATE_SPECULAR_COLOR);
+  glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+
+  for (int f = 0; f < fuentes.size(); ++f) {
+    fuentes.at(f)->activar(f);
+  }
+}
+
+void FuenteLuz::activar(int i) {
+  glEnable(GL_LIGHT0 + i);
+
+  glLightfv(GL_LIGHT0 + i, GL_AMBIENT, colores[0]);
+  glLightfv(GL_LIGHT0 + i, GL_DIFFUSE, colores[1]);
+  glLightfv(GL_LIGHT0 + i, GL_SPECULAR, colores[2]);
+
+  if (posvec[3] == 1) { // => posicional
+    glLightfv(GL_LIGHT0 + i, GL_POSITION, posvec);
+  } else {
+    // Entramos en la modelview
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+    glRotatef(longi, 0.0, 1.0, 0.0);
+    glRotatef(lati, -1.0, 0.0, 0.0);
+    glLightfv(GL_LIGHT0 + i, GL_POSITION, Tupla4f(0, 0, 1, 0));
+
+    glPopMatrix();
+  }
+}
+
+FuentePosicional::FuentePosicional(int index, const Tupla3f& posicion)
+  :FuenteLuz(index, 0, 0, Tupla4f(posicion(X), posicion(Y), posicion(Z), 1)) {
+  for (int i = 0; i < 3; i++)
+    colores[i] = Tupla4f(0.5, 0.7, 0.8, 1);
+}
+
+FuenteDireccional::FuenteDireccional(int index, float longi, float lati)
+  :FuenteLuz(index, longi, lati, Tupla4f(0, 0, 0, 0)) {
+  for (int i = 0; i < 3; i++)
+    colores[i] = Tupla4f(0.7, 0.7, 0.7, 1);
+}
+
+void FuenteDireccional::variarAngulo(unsigned angulo, float incremento) {
+  if (angulo)
+    longi += incremento;
+  else
+    lati += incremento;
+
+  activar(index);
+}
+
+ColeccionFuentesP4::ColeccionFuentesP4() {
+  fuentes.push_back(
+    new FuenteDireccional(0, 0, 0)
+  );
+  fuentes.push_back(
+    new FuentePosicional(1, Tupla3f(2, 10, 3))
+  );
+}
--- FuenteLuz.hpp
+++ FuenteLuz.hpp
@@ -1,15 +1,24 @@
 #ifndef __FUENTE_LUZ
 #define __FUENTE_LUZ
 
+#include <vector>
+#include "aux.hpp"
+#include "tuplasg.hpp"
+
 // ---------------------------------------------------------------------
 // Parámetros que definen una fuente de luz
 
 class FuenteLuz
 {
+protected:
+  int index;
 public:
   Tupla4f  posvec; // posición (si w=1) o vector (w=0)
   Tupla4f  colores[3]; // colores: 0=ambiental, 1=difuso, 2=especular.
   float    longi, lati; // ángulos de rotación (fuente direccional modificable interactivamente)
+
+  FuenteLuz(int index, float longi, float lati, Tupla4f posvec)
+    :index(index), longi(longi), lati(lati), posvec(posvec) {}
 
   void activar(int i); // activa la fuente de luz (con número GL_LIGHT0+i)
 };
@@ -26,13 +35,13 @@ public:
 
 class FuenteDireccional : public FuenteLuz {
 public:
-  FuenteDireccional(float alpha_inicial, float beta_inicial);
+  FuenteDireccional(int index, float longi, float lati);
   void variarAngulo(unsigned angulo, float incremento);  // angulo=0 --> variar alpha, angulo=1 --> variar beta
 };
 
 class FuentePosicional : public FuenteLuz {
 public:
-  FuentePosicional(const Tupla3f& posicion);
+  FuentePosicional(int index, const Tupla3f& posicion);
 };
 
 class ColeccionFuentesP4 : public ColeccionFL {
