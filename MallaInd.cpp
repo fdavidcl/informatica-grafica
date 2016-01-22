4a9cd4e46c51f0983c397d3e3c0c39ea6390315e
David Charte <fdavidcl@outlook.com>
2016-01-22 01:20:42 +0100

    Ajustes mallas

--- MallaInd.cpp
+++ MallaInd.cpp
@@ -8,11 +8,11 @@ la función glDrawElements.
 *************************/
 void MallaInd::visualizar(ContextoVis cv) {
   // Comprobación de parámetros
-  if (cv.modo_vis > 3 || cv.modo_vis < 0) {
+  if (cv.modo_vis > 5 || cv.modo_vis < 0) {
     cv.modo_vis = 2; // Asumimos sólido
   }
 
-  const GLenum modo[] = {GL_POINT, GL_LINE, GL_FILL, GL_FILL};
+  const GLenum modo[] = {GL_POINT, GL_LINE, GL_FILL, GL_FILL, GL_FILL, GL_FILL};
 
   glEnableClientState(GL_VERTEX_ARRAY);
   glVertexPointer(3, GL_FLOAT, 0, &vertex_coords.front());
@@ -21,18 +21,56 @@ void MallaInd::visualizar(ContextoVis cv) {
   if (cv.modo_vis < 3) {
     glColor3f(color(R), color(G), color(B));
     glDrawElements(GL_TRIANGLES, 3 * indexes.size(), GL_UNSIGNED_INT, &indexes.front());
-  } else { // Ajedrez
+  } else if (cv.modo_vis == 3) { // Ajedrez
     for (unsigned i = 0; i < indexes.size(); i++) {
       float tone = 0.8 * (i % 2); // 0.8 if i is odd
       glColor3f(tone, tone, tone);
       glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, &indexes[i]);
     }
+  } else if (cv.modo_vis == 4) {
+    glEnable(GL_LIGHTING);
+    glShadeModel(GL_FLAT);
+
+    glTexCoordPointer(2, GL_FLOAT, 0, &text_coords.front());
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    if (!normales_vertices.empty()) {
+      glNormalPointer(GL_FLOAT, 0, &normales_vertices.front());
+      glEnableClientState(GL_NORMAL_ARRAY);
+    }
+
+    glDrawElements(GL_TRIANGLES, 3 * indexes.size(), GL_UNSIGNED_INT, &indexes.front());
+
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_NORMAL_ARRAY);
+
+    glDisable(GL_LIGHTING);
+  } else if (cv.modo_vis == 5) {
+    glEnable(GL_LIGHTING);
+    glShadeModel(GL_SMOOTH);
+
+    glTexCoordPointer(2, GL_FLOAT, 0, &text_coords.front());
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    if (!normales_vertices.empty()) {
+      glNormalPointer(GL_FLOAT, 0, &normales_vertices.front());
+      glEnableClientState(GL_NORMAL_ARRAY);
+    }
+
+    glDrawElements(GL_TRIANGLES, 3 * indexes.size(), GL_UNSIGNED_INT, &indexes.front());
+
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_NORMAL_ARRAY);
+
+    glDisable(GL_LIGHTING);
   }
 
   glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-static Tupla3f normalizar(Tupla3f t) { return t.normalized(); }
+static Tupla3f normalizar(Tupla3f t) {
+  return (t(X) != 0 || t(Y) != 0 || t(Z) != 0) ? t.normalized() : Tupla3f(1, 0, 0);
+}
 
 void MallaInd::calcularNormales() {
   normales_vertices.resize(vertex_coords.size());
@@ -48,16 +86,20 @@ void MallaInd::calcularNormales() {
       a = q - p,
       b = r - p,
 
-      // Vector normal a la cara
-      normal = a.cross(b).normalized();
+      // Vector ortogonal a la cara
+      ortogonal = a.cross(b);
 
-    normales_caras.push_back(normal);
+    normales_caras.push_back(ortogonal);
 
     // Sumamos ahora el normal a la cara a los vectores que
     // corresponden a los vértices
     for (int v = 0; v < 3; ++v)
-      normales_vertices[(*cara)(v)] = normales_vertices[(*cara)(v)] + normal;
+      normales_vertices[(*cara)(v)] = normales_vertices[(*cara)(v)] + ortogonal;
   }
+
+  // Normalizamos todos los vectores (para máxima eficiencia usamos transform)
+  std::transform(normales_caras.begin(), normales_caras.end(),
+                 normales_caras.begin(), normalizar);
 
   std::transform(normales_vertices.begin(), normales_vertices.end(),
                  normales_vertices.begin(), normalizar);
--- MallaRev.cpp
+++ MallaRev.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 void MallaRevol::construir(unsigned num_perfiles) {
+  num_perfiles = num_perfiles;
   vertex_coords.erase(vertex_coords.begin(), vertex_coords.end());
   indexes.erase(indexes.begin(), indexes.end());
 
@@ -50,7 +51,7 @@ void MallaRevol::construir(unsigned num_perfiles) {
     indexes.push_back(Tupla3i(
       giro * Nv,
       (giro + 1) * Nv,
-      num_perfiles * Nv
+      (num_perfiles + 1) * Nv
     ));
   }
 
@@ -58,9 +59,11 @@ void MallaRevol::construir(unsigned num_perfiles) {
     indexes.push_back(Tupla3i(
       giro * Nv + (Nv - 1),
       (giro + 1) * Nv + (Nv - 1),
-      num_perfiles * Nv + 1
+      (num_perfiles + 1) * Nv + 1
     ));
   }
+
+  calcularNormales();
 }
 
 void MallaRevol::generar_coords_textura() {
@@ -69,22 +72,25 @@ void MallaRevol::generar_coords_textura() {
 
   // Obtenemos las alturas normalizadas de los vértices del perfil
   std::vector<float> alturas;
-  float norma = perfil.back();
+  float norma = perfil.at(perfil.size() - 2) - perfil.at(1);
 
   alturas.push_back(0);
-  for (std::vector<float>::iterator j = perfil.begin() + 3; j != perfil.end(); j += 3) {
-    alturas.push_back((*j - *(j - 1))/norma);
+  for (std::vector<Tupla3f>::iterator j = vertex_coords.begin() + 1; j < vertex_coords.begin() + v_por_perfil; ++j) {
+    alturas.push_back(((*j)(Y) - perfil.at(1))/norma);
   }
 
   // Calculamos las coordenadas de textura
   for (int i = 0; i < total_perfiles; i++) {
-    for (std::vector<float>::iterator j = alturas.begin(); j != alturas.end(); j += 3) {
+    for (std::vector<float>::iterator j = alturas.begin(); j < alturas.end(); ++j) {
       text_coords.push_back(Tupla2f(
         i / (float)(total_perfiles - 1),
         *j
       ));
     }
   }
+
+  for (std::vector<Tupla2f>::iterator i = text_coords.begin(); i < text_coords.end(); ++i)
+    std::cerr << *i << std::endl;
 }
 
 MallaRevol::MallaRevol(const char * filename, unsigned num_perfiles, bool usar_textura, std::string nombre) {
