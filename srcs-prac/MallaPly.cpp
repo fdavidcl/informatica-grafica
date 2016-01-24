643a89187318cf89d4a8c7cb4253c7a9f6e77ba3
David Charte <fdavidcl@outlook.com>
2016-01-24 17:59:23 +0100

    Notas sobre MPL en archivos propios

--- srcs-prac/MallaPly.cpp
+++ srcs-prac/MallaPly.cpp
@@ -1,9 +1,15 @@
+/**********************************************************************
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+**********************************************************************/
+
 #include "MallaPly.hpp"
 #include "file_ply_stl.hpp"
 
 MallaPly::MallaPly(const char * filename, std::string nombre) {
   nombre_obj = nombre;
-  
+
   std::vector<float> vertices;
   std::vector<int> caras;
   ply::read(filename, vertices, caras);
